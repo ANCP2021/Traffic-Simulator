b2352443bd7332d440d6019ef5f0ed902e189e53
Alexander Nemecek <an440818@ohio.edu>
2023-10-18 23:05:00 -0400

    final working simulator

--- TrafficSimulator.py
+++ TrafficSimulator.py
@@ -25,6 +25,11 @@ CAR_WIDTH, CAR_HEIGHT = 30, 20
 CAR_SPEED = 5
 DIRECTION = ''
 
+PEDESTRIAN_COLOR = (128, 0, 128)  # Purple color for pedestrians
+PEDESTRIAN_WIDTH, PEDESTRIAN_HEIGHT = 10, 20
+PEDESTRIAN_SPEED = 2
+PEDESTRIAN_DIRECTION = ''
+
 class Car:
     def __init__(self, x, y, color, speed=CAR_SPEED, direction=DIRECTION):
         self.rect = pygame.Rect(x, y, CAR_WIDTH, CAR_HEIGHT)
@@ -41,7 +46,26 @@ class Car:
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
 
+class Pedestrian:
+    def __init__(self, x, y, color, speed=PEDESTRIAN_SPEED, direction=PEDESTRIAN_DIRECTION):
+        self.rect = pygame.Rect(x, y, PEDESTRIAN_WIDTH, PEDESTRIAN_HEIGHT)
+        self.color = color
+        self.speed = speed
+        self.direction = direction
+
+    def move(self):
+        if self.direction == 'Vertical':
+            self.rect.y += self.speed
+        elif self.direction == 'Horizontal':
+            self.rect.x += self.speed
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+
 cars = []
+pedestrians = []
+
 
 while True:
     for event in pygame.event.get():
@@ -89,6 +113,17 @@ while True:
             cars.append(Car(WIDTH // 2.22,  -CAR_HEIGHT, CAR_COLOR, CAR_SPEED, 'Vertical'))
         if random.randint(1, 100) < 2:
             cars.append(Car(WIDTH // 1.95, HEIGHT, CAR_COLOR, -CAR_SPEED, 'Vertical'))
+
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(WIDTH // 1.75,  -PEDESTRIAN_HEIGHT, PEDESTRIAN_COLOR, PEDESTRIAN_SPEED, 'Vertical'))
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(WIDTH // 1.72, HEIGHT, PEDESTRIAN_COLOR, -PEDESTRIAN_SPEED, 'Vertical'))
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(WIDTH // 2.45,  -PEDESTRIAN_HEIGHT, PEDESTRIAN_COLOR, PEDESTRIAN_SPEED, 'Vertical'))
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(WIDTH // 2.42, HEIGHT, PEDESTRIAN_COLOR, -PEDESTRIAN_SPEED, 'Vertical'))
+
+
             
     elif state_time < GREEN_TIME_NS + YELLOW_TIME_NS:
         pygame.draw.circle(screen, YELLOW, (WIDTH // 2, HEIGHT // 4), 20)
@@ -101,10 +136,10 @@ while True:
         pygame.draw.circle(screen, RED, (WIDTH // 2 - 50, HEIGHT // 2 + 80), 10)
         pygame.draw.circle(screen, RED, (WIDTH // 2 + 50, HEIGHT // 2 + 80), 10)
 
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 - 75, HEIGHT // 2 - 60), 10)
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 - 75, HEIGHT // 2 + 60), 10)
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 + 75, HEIGHT // 2 - 60), 10)
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 + 75, HEIGHT // 2 + 60), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 - 75, HEIGHT // 2 - 60), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 - 75, HEIGHT // 2 + 60), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 + 75, HEIGHT // 2 - 60), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 + 75, HEIGHT // 2 + 60), 10)
 
     elif state_time < GREEN_TIME_NS + YELLOW_TIME_NS + RED_TIME_NS:  
         pygame.draw.circle(screen, RED, (WIDTH // 2, HEIGHT // 4), 20)  
@@ -127,6 +162,15 @@ while True:
             cars.append(Car(WIDTH - CAR_WIDTH, HEIGHT // 2.2, CAR_COLOR, -CAR_SPEED, 'Horizontal'))  
         if random.randint(1, 100) < 2:
             cars.append(Car(CAR_WIDTH, HEIGHT // 1.9, CAR_COLOR, CAR_SPEED, 'Horizontal'))  
+
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(WIDTH - CAR_WIDTH,  HEIGHT // 2.77, PEDESTRIAN_COLOR, -PEDESTRIAN_SPEED, 'Horizontal'))
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(CAR_WIDTH, HEIGHT // 1.72, PEDESTRIAN_COLOR, PEDESTRIAN_SPEED, 'Horizontal'))
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(WIDTH - CAR_WIDTH, HEIGHT // 1.70, PEDESTRIAN_COLOR, -PEDESTRIAN_SPEED, 'Horizontal'))
+        if random.randint(1, 100) < 2:
+            cars.append(Pedestrian(CAR_WIDTH,  HEIGHT // 2.75, PEDESTRIAN_COLOR, PEDESTRIAN_SPEED, 'Horizontal'))
         
     else:  
         pygame.draw.circle(screen, RED, (WIDTH // 2, HEIGHT // 4), 20) 
@@ -134,21 +178,25 @@ while True:
         pygame.draw.circle(screen, YELLOW, (WIDTH // 4, HEIGHT // 2), 20) 
         pygame.draw.circle(screen, YELLOW, (3 * WIDTH // 4, HEIGHT // 2), 20) 
 
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 - 50, HEIGHT // 2 - 85), 10)
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 + 50, HEIGHT // 2 - 85), 10)
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 - 50, HEIGHT // 2 + 80), 10)
-        pygame.draw.circle(screen, LIGHT_GREY, (WIDTH // 2 + 50, HEIGHT // 2 + 80), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 - 50, HEIGHT // 2 - 85), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 + 50, HEIGHT // 2 - 85), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 - 50, HEIGHT // 2 + 80), 10)
+        pygame.draw.circle(screen, RED, (WIDTH // 2 + 50, HEIGHT // 2 + 80), 10)
 
         pygame.draw.circle(screen, RED, (WIDTH // 2 - 75, HEIGHT // 2 - 60), 10)
         pygame.draw.circle(screen, RED, (WIDTH // 2 - 75, HEIGHT // 2 + 60), 10)
         pygame.draw.circle(screen, RED, (WIDTH // 2 + 75, HEIGHT // 2 - 60), 10)
         pygame.draw.circle(screen, RED, (WIDTH // 2 + 75, HEIGHT // 2 + 60), 10)
 
-    
+
     for car in cars:
         car.move()
         car.draw()
+
+    # Move and draw pedestrians
+    for pedestrian in pedestrians:
+        pedestrian.move()
+        pedestrian.draw()
         
-    cars = [car for car in cars if car.rect.right > 0 and car.rect.left < WIDTH and car.rect.bottom > 0 and car.rect.top < HEIGHT]
     
     pygame.display.flip()
